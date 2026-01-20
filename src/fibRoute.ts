1988440625c4957c71de1972643df1165de4b6e7
Daniel Cortina <danny2003cc@gmail.com>
2026-01-19 21:52:55 -0500

    Minor changes to fibroute, will contimue after

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,11 +5,11 @@ import fibonacci from "./fib";
 export default (req, res) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN = Number(fibonacci(parseInt(num)));
+  let result = String(`fibonacci(${num}) is ${fibN}`);
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = String(`fibonacci(${num}) is undefined`);
   }
 
   res.send(result);
